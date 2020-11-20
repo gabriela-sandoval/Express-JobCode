09e5d8cadce537b5edd83011f79da7ea486c95ba
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2020-11-19 20:03:40 -0600

    Creación de proyecto

---
+++ ExpressJob/ExpressJob/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExpressJob.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
