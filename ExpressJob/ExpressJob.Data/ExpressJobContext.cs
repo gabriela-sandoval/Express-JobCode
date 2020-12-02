c774241acfec382c415e1625fabbce162a379258
Tapia <zs17012957@estudiantes.uv.mx>
2020-12-01 22:47:41 -0600

    Se general DataContext & Referencias
    Se generan datacontext 
    Se agregan referencias de proyectos

--- ExpressJob/ExpressJob.Data/ExpressJobContext.cs
+++ ExpressJob/ExpressJob.Data/ExpressJobContext.cs
@@ -1,7 +1,43 @@
-﻿namespace ExpressJob.Data
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ExpressJob.Domain;
+
+
+namespace ExpressJob.Data
 {
     public class ExpressJobContext : IdentityDbContext<ExpressJobUser>
+    
     {
-        
+       public DbSet<ExpressJobUser> ExpressUsers { get; set; }
+
+       public DbSet<Trabajador> Trabajadors { get; set; }
+
+
+        public ExpressJobContext(DbContextOptions<ExpressJobContext> options)
+           : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+
+
+
+
+        }
+
     }
 }
