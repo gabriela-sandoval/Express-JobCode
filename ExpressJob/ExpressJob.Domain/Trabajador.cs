d3bfeac32a85b6f423dbd9194cd8fedc060ebebd
Tapia <zs17012957@estudiantes.uv.mx>
2020-12-03 23:04:36 -0600

    Se agrega la key a la clase trabajador

--- ExpressJob/ExpressJob.Domain/Trabajador.cs
+++ ExpressJob/ExpressJob.Domain/Trabajador.cs
@@ -1,12 +1,15 @@
 ﻿using ExpressJob.Domain.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ExpressJob.Domain
 {
    public  class Trabajador
     {
+        [Key]
+        public string id_trabajador { get; set; }
         public string Descripcion { get; set; }
 
         public string TelefonoFijo { get; set; }
