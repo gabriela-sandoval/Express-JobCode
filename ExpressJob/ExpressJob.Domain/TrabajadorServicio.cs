bd148d14a20a2ed02d618d7818cebfb3c16a6e54
Tapia <zs17012957@estudiantes.uv.mx>
2021-01-12 13:49:56 -0600

    Se cre la clase trabajdorservicio de relación

---
+++ ExpressJob/ExpressJob.Domain/TrabajadorServicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressJob.Domain
+{
+    public class TrabajadorServicio
+    {
+        public int TrabajadorId { get; set; }
+
+        public int ServicioId { get; set; }
+
+        public virtual Trabajador Trabajador { get; set; }
+
+        public virtual Servicio Servicio { get; set; }
+
+
+    }
+}
